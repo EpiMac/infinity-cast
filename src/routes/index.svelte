b10e0f27db20044abc7862470d7de43a68c4524c
Adrien Navratil <adrien1975@live.fr>
2021-04-24 17:46:35 +0900

    Finished index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,10 @@
     import { user } from '$lib/auth';
     // import { receive, keys } from '$lib/ui/crossfade';
 
+    import create from '../assets/images/menu_create.svg?raw';
+    import pending from '../assets/images/menu_pending.svg?raw';
+    import sent from '../assets/images/menu_sent.svg?raw';
+
     /**
      * @type {import('@sveltejs/kit').Load}
      */
@@ -24,6 +28,12 @@
 
     import Footer from '$lib/components/Footer.svelte';
     import Logo from '$lib/components/Logo.svelte';
+
+    const entries = [
+        { path: '/new/author', label: 'Créer une nouvelle annonce', icon: create },
+        { path: '/list/pending', label: 'Voir les annonces en attente d\'envoi', icon: pending },
+        { path: '/list/sent', label: 'Consulter l\'historique d\'envoi', icon: sent }
+    ];
 </script>
 
 <svelte:head>
@@ -38,8 +48,14 @@
     <a id="logout" href="/auth/logout" sveltekit:prefetch in:fade={{ delay: 200 }}>Se déconneter</a>
 </div>
 
-<div in:fade={{ delay: 400, duration: 175 }}>
-    Salut :)
+<div id="menu" in:fade={{ delay: 400, duration: 175 }}>
+    {#each entries as entry}
+        <a class="entry card clickable opaque" sveltekit:prefetch href={entry.path}>
+            <div class="icon">{@html entry.icon}</div>
+            <div class="label">{@html entry.label}</div>
+            <div>&gt;</div>
+        </a>
+    {/each}
 </div>
 
 <Footer />
@@ -64,6 +80,35 @@
 
             font-size: 22px;
             font-weight: 500;
+        }
+    }
+
+    #menu {
+        flex-direction: column;
+
+        .entry {
+            width: 700px;
+            height: 105px;
+
+            display: flex;
+            align-items: center;
+
+            padding: 0 25px;
+
+            &:not(:last-child) {
+                margin-bottom: 20px;
+            }
+
+            .icon {
+                transform: translateY(3px); // When using margin, a glitch will appear on hovering
+            }
+
+            .label {
+                flex-grow: 1;
+                font-size: 22px;
+
+                margin-left: 25px;
+            }
         }
     }
 
